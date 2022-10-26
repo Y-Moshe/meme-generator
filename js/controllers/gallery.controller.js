27cdc2dcd5546418540a0b8ce9c4acdca3f47d8f
Y-Moshe <moshe.nehemiah@gmail.com>
2022-10-26 15:23:48 +0300

    gallery and meme controllers

---
+++ js/controllers/gallery.controller.js
@@ -0,0 +1,21 @@
+function renderGallery() {
+  const gallery = getImgs().map(renderGalleryItem)
+
+  document.querySelector('.gallery')
+    .innerHTML = gallery.join('')
+}
+
+function renderGalleryItem({ id, url, keywords }) {
+  const badges = keywords.map(key => `<span class="gallery-badge">${key}</span>`)
+
+  return `
+    <div class="gallery-item" onclick="onImgSelect('${id}')">
+      <img src="${url}" alt="${id} img" class="gallery-item-img" />
+      <div class="gallery-item-keywords">${badges.join('')}</div>
+    </div>
+  `
+}
+
+function onImgSelect(id) {
+  console.log('onImgSelect');
+}
---
+++ js/controllers/meme.controller.js
@@ -0,0 +1,6 @@
+let gCanvas = document.createElement('canvas')
+let gCtx = gCanvas.getContext('2d')
+
+function renderMeme() {
+  const meme = getMeme()
+}
