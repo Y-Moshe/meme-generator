5a10e1cd024f257736aa59d5b9e9719e258f3809
Y-Moshe <moshe.nehemiah@gmail.com>
2022-10-26 19:59:50 +0300

    update image path

--- js/controllers/gallery.controller.js
+++ js/controllers/gallery.controller.js
@@ -7,7 +7,7 @@ function onInit() {
 function initImgs() {
   gImgs = []
   for (let i = 1; i <= 18; i++) {
-    gImgs.push(createMemeImg(`img/${i}.jpg`, ['test', 'gol']))
+    gImgs.push(createMemeImg(`assets/img/${i}.jpg`, ['test', 'gol']))
   }
 }
 
