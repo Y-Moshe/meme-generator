a4a5b40568a84b1488939e57156fd2a01250154b
Y-Moshe <moshe.nehemiah@gmail.com>
2022-10-27 17:32:22 +0300

    navigation and keywords render

--- js/controllers/gallery.controller.js
+++ js/controllers/gallery.controller.js
@@ -1,7 +1,15 @@
+const RENDER_COMPONENTS = {
+  GALLERY: '.gallery-container',
+  EDITOR: '.meme-generator',
+  MY_MEMES: '.my-memes',
+  ABOUT: '.about'
+}
+
 function onInit() {
   initImgs()
-  renderGallery()
+  renderKeywordsList()
   initCanvas()
+  render(RENDER_COMPONENTS.GALLERY)
 }
 
 function initImgs() {
@@ -9,6 +17,17 @@ function initImgs() {
   for (let i = 1; i <= 18; i++) {
     gImgs.push(createMemeImg(`assets/img/${i}.jpg`, ['test', 'gol']))
   }
+}
+
+function renderKeywordsList() {
+  const keywords = MEME_KEYWORDS.map(renderKeywordItem)
+
+  document.querySelector('.filters .keywords')
+    .innerHTML = keywords.join(' ')
+}
+
+function renderKeywordItem(word) {
+  return `<span class="word">${word}</span>`
 }
 
 function renderGallery() {
@@ -30,10 +49,44 @@ function renderGalleryItem({ id, url, keywords }) {
 }
 
 function onImgSelect(id) {
-  // Hide Gallery
-  document.querySelector('.gallery-container')
-    .setAttribute('hidden', true)
-
   setSelectedImgId(id)
-  renderMeme()
+  render(RENDER_COMPONENTS.EDITOR)
+}
+
+function render(component) {
+  switch (component) {
+    case RENDER_COMPONENTS.GALLERY:
+      const elLink = document.querySelector('.main-nav ul a.gallery-lnk')
+      setActiveNavLink(elLink)
+      renderGallery()
+      break;
+    case RENDER_COMPONENTS.EDITOR:
+      renderMeme()
+      break;
+    case RENDER_COMPONENTS.MY_MEMES:
+      const elMyMemes = document.querySelector('.main-nav ul a.my-memes-lnk')
+      setActiveNavLink(elMyMemes)
+      // TO-DO
+      break;
+    case RENDER_COMPONENTS.ABOUT:
+        const elAbout = document.querySelector('.main-nav ul a.about-lnk')
+        setActiveNavLink(elAbout)
+      break;
+  }
+
+  // Display and hide relevent component
+  document.querySelector('main > section:not(.hide)')
+    ?.classList.add('hide')
+  document.querySelector(`main > section${component}`)
+    ?.classList.remove('hide')
+}
+
+function onNavigateChange(component) {
+  render(component)
+}
+
+function setActiveNavLink(elActiveLink) {
+  document.querySelector('.main-nav ul a.active-link')
+    ?.classList.remove('active-link')
+  elActiveLink?.classList.add('active-link')
 }
