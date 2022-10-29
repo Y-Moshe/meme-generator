7ddfaebc444ac5f3b3fccfeb49147c8f4953c56d
Y-Moshe <moshe.nehemiah@gmail.com>
2022-10-29 22:26:51 +0300

    utils cleanup

--- js/services/utils.service.js
+++ js/services/utils.service.js
@@ -32,86 +32,26 @@ function getRandomColor() {
   return color
 }
 
-function updateQueryParam(key, value) {
-  const queryParams = new URLSearchParams(window.location.search);
-  queryParams.set(key, value);
-
-  history.pushState(null, null, '?' + encodeURI(queryParams.toString()));
-}
-
-function isNumberInString(str) {
-  return /\d/.test(str)
-}
-
-function myFetch(url, cb) {
-  const XHR = new XMLHttpRequest()
-  XHR.onreadystatechange = () => {
-    if (XHR.readyState === XMLHttpRequest.DONE && XHR.status === 200) {
-      const res = JSON.parse(XHR.responseText)
-      cb(res)
-    }
-  }
-
-  XHR.open('GET', url)
-  XHR.send()
-}
-
-const debounce = (func, wait) => {
-  let timeout
-
-  return (...args) => {
-    const later = () => {
-      clearTimeout(timeout)
-      func(...args)
-    }
-
-    clearTimeout(timeout)
-    timeout = setTimeout(later, wait)
-  }
-}
-
-function capitlaize(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1)
-}
-
-function convertArrayToCSVContent(arr) {
-  const csvHeader  = Object.keys(arr[0]).join() + '\n'
-  const csvContent = arr.map(item => {
-    const line = Object.values(item).join()
-    return line + '\n'
-  })
-
-  return csvHeader + csvContent.join('')
-}
-
 function isRectClicked(currX, currY, rectX, rectY, width, height) {
-  // Check if the click coordinates are inside the react coordinates
   return currX > rectX && currX < rectX + width &&
     currY > rectY && currY < rectY + height
 }
 
-//Check if the click is inside the circle 
 function isCircleClicked(currX, currY, circleX, circleY, radius) {
-  // Calc the distance between two dots
   const distance = Math.sqrt((circleX - currX) ** 2 + (circleY - currY) ** 2)
-  //If its smaller then the radius of the circle we are inside
   return distance <= radius
 }
 
-const TOUCH_EVS = ['touchstart', 'touchmove', 'touchend']
+const TOUCH_EVENTS = ['touchstart', 'touchmove', 'touchend']
 function getEventPos(ev) {
-  // Gets the offset pos, the default pos
   let pos = {
     x: ev.offsetX,
     y: ev.offsetY
   }
 
-  if (TOUCH_EVS.includes(ev.type)) {
-    // soo we will not trigger the mouse ev
+  if (TOUCH_EVENTS.includes(ev.type)) {
     ev.preventDefault()
-    // Gets the first touch point
     ev = ev.changedTouches[0]
-    // Calc the right pos according to the touch screen
     pos = {
       x: ev.pageX - ev.target.offsetLeft - ev.target.clientLeft,
       y: ev.pageY - ev.target.offsetTop - ev.target.clientTop
